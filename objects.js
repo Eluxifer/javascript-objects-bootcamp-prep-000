c8c8a4b43a0f6935dfcba98b241b384c62f50151
Jeison Rodriguez <jeisonrodriguez94@outlook.com>
2018-12-04 04:34:26 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {
+object: "object"
+}
+
+function updatePlaylist(playlist, 'Jon Bon Jovi', 'Living on a Prayer'){
+
+
+}
+function removeFromPlaylist(playlist, 'Jon Bon Jovi'){
+  return playlist
+}
